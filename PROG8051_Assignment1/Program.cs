fc56e224fbf3626f25c951ccfae8d56b0e87292b
sachinkallukadavilreji <sachinkr690@gmail.com>
2024-02-02 19:21:11 -0500

    Added if condition for accepting pet type and name.

--- PROG8051_Assignment1/Program.cs
+++ PROG8051_Assignment1/Program.cs
@@ -14,7 +14,7 @@ namespace CSharpTutorials
             /*            Console.WriteLine(message);
             */
             string petName;
-            string petType;
+            string petType="";
             
             Console.WriteLine("Please choose a type of pet:\n1. Cat\n2.Dog\n3.Rabbit");
             Console.WriteLine("User Input: ");
@@ -22,25 +22,42 @@ namespace CSharpTutorials
 
             /*            Accepting user input from console and assigning pet name according to user input.
             */
-            if (choice == 1)
-            {
-                petType = "Cat";
-            } 
-            else if(choice == 2)
-            {
-                petType = "Dog";
-            }
-            else if(choice == 3)
-            {
-                petType = "Rabbit";
-            }
-            else { Console.WriteLine("Enter a valid input !!"); }
+            do {
+                if (choice == 1)
+                {
+                    petType = "Cat";
+                }
+                else if (choice == 2)
+                {
+                    petType = "Dog";
+                }
+                else if (choice == 3)
+                {
+                    petType = "Rabbit";
+                }
+                else { 
+                    Console.WriteLine("Enter a valid input !!");
+                    Console.WriteLine("Please choose a type of pet:\n1. Cat\n2.Dog\n3.Rabbit");
+                    Console.WriteLine("User Input: ");
+                    choice = Convert.ToInt32(Console.ReadLine());
+                }
+
+            } while (choice!=1 || choice != 2 || choice != 3);
+
+            Console.WriteLine("You have chossen a " + petType + ". What would you like to name your pet");
+            Console.WriteLine("User Input: ");
+            petName = (Console.ReadLine());
+            Console.WriteLine("Welcome, " + petType + "! Lets take good care of him");
 
 
-                
 
 
-         
+
+
+
+
+
+
         }
     }
 }
